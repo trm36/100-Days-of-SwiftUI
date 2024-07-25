50669b0204441f980eeeece9c1125be4c9ef00ad
Taylor Mott <trm36@humboldt.edu>
2024-07-25 01:03:16 -0700

    DAY 20 - WeFlag

---
+++ 3 - WeFlag/WeFlag/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WeFlag
+//
+//  Created by Taylor on 7/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+        Color.red
+            .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 3 - WeFlag/WeFlag/WeFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeFlagApp.swift
+//  WeFlag
+//
+//  Created by Taylor on 7/24/24.
+//
+
+import SwiftUI
+
+@main
+struct WeFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
