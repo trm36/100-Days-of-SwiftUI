65c428cbe4e68c16673ed1c0d89f4f1c4f0c13b0
Taylor Mott <trm36@humboldt.edu>
2024-09-28 11:00:20 -0600

    DAY 45 - WeNavigate
    - Toolbar button placement
    - Edit nav title
    - Style navigation bar (color, hide)

--- 12 - WeNavigate/WeNavigate/ContentView.swift
+++ 12 - WeNavigate/WeNavigate/ContentView.swift
@@ -9,14 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var pathStore = PathStore()
+    @State private var title = "SwiftUI"
 
     var body: some View {
-        NavigationStack(path: $pathStore.path) {
-            DetailView(number: 0, path: $pathStore.path)
-                .navigationDestination(for: Int.self) { i in
-                    DetailView(number: i, path: $pathStore.path)
+
+        NavigationStack {
+            List(0..<100) { i in
+                Text("Row \(i)")
+            }
+            .navigationTitle($title)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbarBackground(.blue)
+            .toolbarColorScheme(.dark)
+            .toolbar {
+                ToolbarItemGroup(placement: .topBarLeading) {
+                    Button("Tap Me") {
+                        // button action here
+                    }
+
+                    Button("Tap Me 2") {
+                        // button action here
+                    }
                 }
+            }
+//            .toolbar(.hidden, for: .navigationBar)
         }
+
+
+//        NavigationStack(path: $pathStore.path) {
+//            DetailView(number: 0, path: $pathStore.path)
+//                .navigationDestination(for: Int.self) { i in
+//                    DetailView(number: i, path: $pathStore.path)
+//                }
+//                .navigationTitle("Title goes here")
+//                .navigationBarTitleDisplayMode(.inline)
+//
+//
+//
+//
+//        }
     }
 }
 
