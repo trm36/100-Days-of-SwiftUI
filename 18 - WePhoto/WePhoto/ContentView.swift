b3e4c1ecd4e74392584f2cffa2d35e033543b60e
Taylor Mott <trm36@humboldt.edu>
2024-11-15 14:52:51 -0700

    DAY 65 - WePhoto
    - PhotosPicker
    - CoreImage
    - ContentUnavailableView
    - onChange

--- 18 - WePhoto/WePhoto/ContentView.swift
+++ 18 - WePhoto/WePhoto/ContentView.swift
@@ -7,96 +7,78 @@
 
 import CoreImage
 import CoreImage.CIFilterBuiltins
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
-    @State private var image: Image?
+    @State private var selectedItem: PhotosPickerItem?
+    @State private var processedImage: Image?
+
+    @State private var currentFilter = CIFilter.sepiaTone()
+    @State private var filterIntensity: Float = 0.5
+    private let context = CIContext()
 
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
+        NavigationStack {
+            VStack {
+                Spacer()
 
-            ContentUnavailableView {
-                Label("No snippets", systemImage: "swift")
-            } description: {
-                Text("You don't have any saved snippets yet.")
-            } actions: {
-                Button("Create Snippet") {
-                    // create a snippet
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                    }
                 }
-                .buttonStyle(.borderedProminent)
+                .buttonStyle(.plain)
+                .onChange(of: selectedItem, loadImage)
+
+                Spacer()
+
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity, applyProcessing)
+                }
+                .padding(.vertical)
+
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+                    Spacer()
+
+                    //share
+                }
             }
-        }
-        .onAppear(perform: loadImage)
-    }
-
-    func loadImage() {
-        let filter = Filter.twirl
-        let inputImage = UIImage(resource: .example)
-        let beginImage = CIImage(image: inputImage)
-
-        let context = CIContext()
-
-        switch filter {
-        case .sepia:
-            let currentFilter = CIFilter.sepiaTone()
-            currentFilter.inputImage = beginImage
-            currentFilter.intensity = 0.5
-
-            setImage(filter: currentFilter)
-        case .pixelate:
-            let currentFilter = CIFilter.pixellate()
-            currentFilter.inputImage = beginImage
-            currentFilter.scale = 100
-
-            setImage(filter: currentFilter)
-        case .twirlModern:
-            let currentFilter = CIFilter.twirlDistortion()
-            currentFilter.inputImage = beginImage
-            currentFilter.radius = 1000
-            currentFilter.center = CGPoint(x: inputImage.size.width / 2, y: inputImage.size.height / 2)
-
-            setImage(filter: currentFilter)
-
-        case .twirl:
-            let currentFilter = CIFilter.twirlDistortion()
-            currentFilter.inputImage = beginImage
-
-            let amount = 1.0
-
-            let inputKeys = currentFilter.inputKeys
-
-            if inputKeys.contains(kCIInputIntensityKey) {
-                currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
-            }
-            
-            if inputKeys.contains(kCIInputRadiusKey) {
-                currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey)
-            }
-            
-            if inputKeys.contains(kCIInputScaleKey) {
-                currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)
-            }
-
-            setImage(filter: currentFilter)
+            .padding([.horizontal, .bottom])
+            .navigationTitle("WePhoto")
         }
     }
 
-    func setImage(filter: CIFilter) {
-        let context = CIContext()
-        guard let outputImage = filter.outputImage else { return }
+    private func loadImage() {
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self),
+                  let inputImage = UIImage(data: imageData) else { return }
+
+            let beginImage = CIImage(image: inputImage)
+            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+            applyProcessing()
+        }
+    }
+
+    private func applyProcessing() {
+        currentFilter.intensity = filterIntensity
+
+        guard let outputImage = currentFilter.outputImage else { return }
         guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+
         let uiImage = UIImage(cgImage: cgImage)
-        image = Image(uiImage: uiImage)
+        processedImage = Image(uiImage: uiImage)
     }
 
-    enum Filter {
-        case sepia
-        case pixelate
-        case twirlModern
-        case twirl
+    private func changeFilter() {
+
     }
 }
 
--- 18 - WePhoto/WePhoto/WePhotoApp.swift
+++ 18 - WePhoto/WePhoto/WePhotoApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct WePhotoApp: App {
     var body: some Scene {
         WindowGroup {
-            LessonView3()
+            ContentView()
         }
     }
 }
