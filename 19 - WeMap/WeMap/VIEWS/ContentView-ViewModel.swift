9285870ddf3687d5390693862cb601a0a2ec9cbb
Taylor Mott <trm36@humboldt.edu>
2025-02-14 11:35:21 -0700

    DAY 73 - WeMap - CHALLENGES PT1

--- 19 - WeMap/WeMap/VIEWS/ContentView-ViewModel.swift
+++ 19 - WeMap/WeMap/VIEWS/ContentView-ViewModel.swift
@@ -9,6 +9,7 @@ import CoreLocation
 import Foundation
 import LocalAuthentication
 import MapKit
+import SwiftUI
 
 extension ContentView {
     
@@ -16,6 +17,9 @@ extension ContentView {
     class ViewModel {
         private(set) var locations: [Location]
         var selectedPlace: Location?
+        var mapAppearance: MapAppearance = .standard
+        var showingMissingAuthenticationErrorAlert: Bool = false
+        var showingFailedAuthenticationErrorAlert: Bool = false
 
         init() {
             do {
@@ -32,17 +36,17 @@ extension ContentView {
             let context = LAContext()
             var error: NSError?
 
-            guard context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
-                // >< no face/touch ID.
+            guard context.canEvaluatePolicy(.deviceOwnerAuthentication, error: &error) else {
+                showingMissingAuthenticationErrorAlert = true
                 return
             }
 
             let reason = "Please authenticate to unlock your places." // The string in our code is used for Touch ID, whereas the string in Info.plist is used for Face ID.
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+            context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: reason) { success, authenticationError in
                 if success {
                     self.isUnlocked = true
                 } else {
-                    // >< error
+                    self.showingFailedAuthenticationErrorAlert = true
                 }
             }
         }
@@ -69,6 +73,30 @@ extension ContentView {
             guard let index = locations.firstIndex(of: location) else { return }
             locations[index] = location
             save()
+        }
+
+        // MARK: - MAP STYLE
+        enum MapAppearance: String {
+            case standard
+            case hybrid
+
+            var mapStyle: MapStyle {
+                switch self {
+                case .standard:
+                    return .standard
+                case .hybrid:
+                    return .hybrid
+                }
+            }
+        }
+
+        func toggleMapAppearance() {
+            switch mapAppearance {
+            case .standard:
+                mapAppearance = .hybrid
+            case .hybrid:
+                mapAppearance = .standard
+            }
         }
     }
 }
--- 19 - WeMap/WeMap/VIEWS/ContentView.swift
+++ 19 - WeMap/WeMap/VIEWS/ContentView.swift
@@ -22,35 +22,61 @@ struct ContentView: View {
     var body: some View {
         if viewModel.isUnlocked {
             MapReader { proxy in
-                Map(initialPosition: startPosition) {
-                    ForEach(viewModel.locations) { location in
-                        //                        Marker(location.name, coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
-                        Annotation(location.name, coordinate: location.coordinate) {
-                            Image(systemName: "star.circle")
-                                .resizable()
-                                .foregroundStyle(.red)
-                                .frame(width: 44.0, height: 44.0)
-                                .background(.white)
-                                .clipShape(.circle)
-                                .simultaneousGesture(
-                                    LongPressGesture(minimumDuration: 1.0)
-                                        .onEnded { _ in
-                                            viewModel.selectedPlace = location
-                                        }
-                                )
+                ZStack {
+                    Map(initialPosition: startPosition) {
+                        ForEach(viewModel.locations) { location in
+                            //                        Marker(location.name, coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
+                            Annotation(location.name, coordinate: location.coordinate) {
+                                Image(systemName: "star.circle")
+                                    .resizable()
+                                    .foregroundStyle(.red)
+                                    .frame(width: 44.0, height: 44.0)
+                                    .background(.white)
+                                    .clipShape(.circle)
+                                    .simultaneousGesture(
+                                        LongPressGesture(minimumDuration: 1.0)
+                                            .onEnded { _ in
+                                                viewModel.selectedPlace = location
+                                            }
+                                    )
+                            }
                         }
                     }
-                }
-                .onTapGesture { position in
-                    print("Tapped at screen possition \(position)")
-                    guard let coordinate = proxy.convert(position, from: .local) else { return }
-                    print("Tapped at coordinate \(coordinate)")
-                    viewModel.addLocation(at: coordinate)
-                }
-                .sheet(item: $viewModel.selectedPlace) { place in
-                    EditAnnotationView(location: place) { newLocation in
-                        viewModel.updateLocation(location: newLocation)
+                    .mapStyle(viewModel.mapAppearance.mapStyle)
+                    .onTapGesture { position in
+                        print("Tapped at screen possition \(position)")
+                        guard let coordinate = proxy.convert(position, from: .local) else { return }
+                        print("Tapped at coordinate \(coordinate)")
+                        viewModel.addLocation(at: coordinate)
                     }
+                    .sheet(item: $viewModel.selectedPlace) { place in
+                        EditAnnotationView(location: place) { newLocation in
+                            viewModel.updateLocation(location: newLocation)
+                        }
+                    }
+
+                    VStack {
+                        HStack {
+                            Spacer()
+
+                            VStack {
+                                Button(action: viewModel.toggleMapAppearance) {
+                                    if viewModel.mapAppearance == .hybrid {
+                                        Image(systemName: "map.fill")
+                                    } else {
+                                        Image(systemName: "map")
+                                    }
+                                }
+                            }
+                            .padding()
+                            .background(.gray)
+                            .foregroundStyle(.white)
+                            .clipShape(.rect(cornerRadius: 12.0))
+                        }
+
+                        Spacer()
+                    }
+                    .padding()
                 }
             }
         } else {
@@ -59,6 +85,19 @@ struct ContentView: View {
                 .background(.blue)
                 .foregroundStyle(.white)
                 .clipShape(.capsule)
+                .alert(isPresented: Binding<Bool>(
+                    get: { self.viewModel.showingMissingAuthenticationErrorAlert },
+                    set: { self.viewModel.showingMissingAuthenticationErrorAlert = $0 }
+                )) {
+                    Alert(title: Text("Enable Device Authentication"))
+                }
+                .alert(isPresented: Binding<Bool>(
+                    get: { self.viewModel.showingFailedAuthenticationErrorAlert },
+                    set: { self.viewModel.showingFailedAuthenticationErrorAlert = $0 }
+                )) {
+                    Alert(title: Text("Failed Device Authentication"),
+                          message: Text("Please try authentication again."))
+                }
         }
     }
 }
