add44d2b6087fb03b7f543a30f3bfee8139fa7a8
Taylor Mott <trm36@humboldt.edu>
2025-02-19 12:23:17 -0700

    DAY 77 - CHALLENGE - WeTag
    Adds Map

--- 21 - WeTag/WeTag/VIEWS/AddPersonView.swift
+++ 21 - WeTag/WeTag/VIEWS/AddPersonView.swift
@@ -5,12 +5,14 @@
 //  Created by Taylor on 2025-02-17.
 //
 
+import MapKit
 import PhotosUI
 import SwiftUI
 
 struct AddPersonView: View {
     @Environment(\.dismiss) var dismiss
     var onSave: (Person) -> Void
+    let locationFetcher = LocationFetcher()
 
     @State private var selectedItem: PhotosPickerItem?
     @State private var processedData: Data?
@@ -18,12 +20,29 @@ struct AddPersonView: View {
 
     @State private var name: String = ""
     @State private var meetingNote: String = ""
+    @State private var location: CLLocationCoordinate2D?
+    @State private var addLocationToggleIsOn: Bool = false
 
     @State private var missingInformationAlertIsPresented: Bool = false
+    @State private var missingLocationAlertIsPresented: Bool = false
+
+    var startPosition: MapCameraPosition {
+        guard let location else {
+            return MapCameraPosition.region(MKCoordinateRegion(center: .init(latitude: 37.789467, longitude: -122.416772), span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)))
+        }
+
+        return MapCameraPosition.region(
+            MKCoordinateRegion(
+                center: location,
+                span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
+            )
+        )
+    }
 
     var body: some View {
         NavigationStack {
             List {
+                // PHOTO
                 Section {
                     Color.clear
                         .aspectRatio(1.0, contentMode: .fill)
@@ -43,6 +62,7 @@ struct AddPersonView: View {
                         )
                 }
 
+                // TEXT
                 Section {
                     TextField(text: $name) {
                         Text("Name")
@@ -55,14 +75,60 @@ struct AddPersonView: View {
                     }
                     .font(.subheadline)
                 }
+
+                // LOCATION
+                Section {
+                    HStack {
+                        Toggle("Add Location?", isOn: $addLocationToggleIsOn)
+                            .onChange(of: addLocationToggleIsOn, startLocationFetch)
+                    }
+                }
+
+                if addLocationToggleIsOn {
+                    Section {
+                        if location != nil {
+                            HStack {
+                                ActiveIndicator()
+                                Text("Location Set")
+                            }
+                        } else {
+                            Text("No location set")
+                                .foregroundStyle(.secondary)
+                                .font(.callout)
+                                .italic()
+                        }
+
+                        Button("Set Current Location") {
+                            location = locationFetcher.lastKnownLocation
+                        }
+                    }
+                }
+
+
+                if let location {
+                    Section {
+                        Map(initialPosition: startPosition, interactionModes: [.zoom, .rotate, .pitch]) {
+                            Marker(name, coordinate: location)
+                        }
+                        .aspectRatio(1.0, contentMode: .fit)
+                        .listRowInsets(EdgeInsets())
+                    }
+                }
             }
             .navigationTitle("Add Person")
             .toolbar {
                 Button("Save") {
                     if let data = processedData, !name.isEmpty, !meetingNote.isEmpty {
-                        let newPerson = Person(name: name, meetingNote: meetingNote, image: data, id: UUID())
-                        onSave(newPerson)
-                        dismiss()
+                        if addLocationToggleIsOn {
+                            guard let location = location else {
+                                missingLocationAlertIsPresented = true
+                                return
+                            }
+
+                            saveWithLocation(name: name, meetingNote: meetingNote, imageData: data, location: location)
+                        } else {
+                            saveWithoutLocation(name: name, meetingNote: meetingNote, imageData: data)
+                        }
                     } else {
                         missingInformationAlertIsPresented = true
                     }
@@ -72,8 +138,35 @@ struct AddPersonView: View {
                 }, message: {
                     Text("Please ensure you have a photo, name, and meeting note entered for this person.")
                 })
+                .alert("No Location Set", isPresented: $missingLocationAlertIsPresented, actions: {
+
+                    Button("Save Without Location", role: .destructive) {
+                        guard let data = processedData else {
+                            missingInformationAlertIsPresented = true
+                            return
+                        }
+                        saveWithoutLocation(name: name, meetingNote: meetingNote, imageData: data)
+                    }
+
+                    Button("OK", role: .cancel) {}
+
+                }, message: {
+                    Text("You've indicated you'd like to save the location with this person, but have not set the location. Please save without location or set the location and try again.")
+                })
             }
         }
+    }
+
+    private func saveWithLocation(name: String, meetingNote: String, imageData: Data, location: CLLocationCoordinate2D?) {
+        let newPerson = Person(name: name, meetingNote: meetingNote, image: imageData, location: location)
+        onSave(newPerson)
+        dismiss()
+    }
+
+    private func saveWithoutLocation(name: String, meetingNote: String, imageData: Data) {
+        let newPerson = Person(name: name, meetingNote: meetingNote, image: imageData)
+        onSave(newPerson)
+        dismiss()
     }
 
     private func loadImage() {
@@ -84,6 +177,15 @@ struct AddPersonView: View {
             processedData = imageData
             processedImage = Image(uiImage: inputImage)
         }
+    }
+
+    private func startLocationFetch() {
+        locationFetcher.start()
+    }
+
+    private func getCurrentLocation() {
+        guard let location = locationFetcher.lastKnownLocation else { print("location not found"); return }
+        print("your location is \(location)")
     }
 }
 
