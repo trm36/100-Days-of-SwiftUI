dc7a7b92f6c4ac98d18e87f4ba64c0ba2084e393
Taylor Mott <trm36@humboldt.edu>
2025-02-21 00:57:41 -0700

    DAY 83 - PROJECT - WeProspect
    - Generate and display user’s QR Code
    - Scans QR codes
    - Swipe Row Actions, Contacted<—>Uncontacted, Delete
    - Multi Select List Rows, Delete

--- 22 - WeProspect/WeProspect/VIEWS/MeView.swift
+++ 22 - WeProspect/WeProspect/VIEWS/MeView.swift
@@ -5,14 +5,53 @@
 //  Created by Taylor on 2025-02-20.
 //
 
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct MeView: View {
+    @AppStorage("name") private var name = ""
+    @AppStorage("emailAddress") private var emailAddress = ""
+
     var body: some View {
         NavigationStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                .navigationTitle("Me")
+            Form {
+                Section {
+                    TextField("Name", text: $name)
+                        .textContentType(.name)
+                        .font(.title)
+                        .frame(height: 50.0)
+                    
+                    TextField("Email address", text: $emailAddress)
+                        .textContentType(.emailAddress)
+                        .font(.title)
+                        .frame(height: 50.0)
+                }
+
+                if !name.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty && !emailAddress.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+                    Section {
+                        Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                            .interpolation(.none)
+                            .resizable()
+                            .scaledToFit()
+                    }
+                }
+            }
+            .navigationTitle("Your code")
         }
+    }
+
+    // MARK: - QR CODES
+    let context = CIContext()
+    let qrFilter = CIFilter.qrCodeGenerator()
+
+    private func generateQRCode(from string: String) -> UIImage {
+        qrFilter.message = Data(string.utf8)
+
+        guard let outputImage = qrFilter.outputImage, let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {
+            return UIImage(systemName: "xmark.circle") ?? UIImage()
+        }
+
+        return UIImage(cgImage: cgImage)
     }
 }
 
--- 22 - WeProspect/WeProspect/VIEWS/ProspectsView.swift
+++ 22 - WeProspect/WeProspect/VIEWS/ProspectsView.swift
@@ -5,6 +5,7 @@
 //  Created by Taylor on 2025-02-20.
 //
 
+import CodeScanner
 import SwiftData
 import SwiftUI
 
@@ -12,11 +13,11 @@ struct ProspectsView: View {
     @Query(sort: \Prospect.name) var prospects: [Prospect]
     @Environment(\.modelContext) var modelContext
 
-    enum FilterType {
-        case none, contacted, uncontacted
-    }
+    @State private var selectedProspects = Set<Prospect>()
+    @State private var isShowingScanner = false
 
     let filter: FilterType
+
     var title: String {
         switch filter {
         case .none:
@@ -28,6 +29,8 @@ struct ProspectsView: View {
         }
     }
 
+
+    // MARK: - INITIALIZERS
     init(filter: FilterType) {
         self.filter = filter
 
@@ -44,24 +47,90 @@ struct ProspectsView: View {
         }
     }
 
+    // MARK: - BODY
     var body: some View {
         NavigationStack {
-            List(prospects) { prospect in
+            List(prospects, selection: $selectedProspects) { prospect in
                 VStack(alignment: .leading) {
                     Text(prospect.name)
                         .font(.headline)
                     Text(prospect.emailAddress)
                         .foregroundStyle(.secondary)
                 }
+                .tag(prospect)
+                .swipeActions {
+                    if prospect.isContacted {
+                        Button("Mark Uncontacted", systemImage: "person.crop.circle.badge.xmark") {
+                            prospect.isContacted.toggle()
+                        }
+                        .tint(.blue)
+
+                        Button("Delete", systemImage: "trash", role: .destructive) {
+                            modelContext.delete(prospect)
+                        }
+                    } else {
+                        Button("Mark Contacted", systemImage: "person.crop.circle.fill.badge.checkmark") {
+                            prospect.isContacted.toggle()
+                        }
+                        .tint(.green)
+
+                        Button("Delete", systemImage: "trash", role: .destructive) {
+                            modelContext.delete(prospect)
+                        }
+                    }
+                }
             }
             .navigationTitle(title)
             .toolbar {
-                Button("Scan", systemImage: "qrcode.viewfinder") {
-                    let prospect = Prospect(name: "Paul Hudson", emailAddress: "paul@hackingwithswift.com", isContacted: false)
-                    modelContext.insert(prospect)
+                ToolbarItem(placement: .topBarLeading) {
+                    EditButton()
+                }
+
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Scan", systemImage: "qrcode.viewfinder") {
+                        isShowingScanner = true
+                    }
+                    .sheet(isPresented: $isShowingScanner) {
+                        CodeScannerView(codeTypes: [.qr], simulatedData: "Paul Hudson\npaul@hackingwithswift.com", completion: handleScan)
+                    }
+                }
+
+                if !selectedProspects.isEmpty {
+                    ToolbarItem(placement: .bottomBar) {
+                        Button("Delete Selected", action: delete)
+                    }
                 }
             }
         }
+    }
+
+    // MARK: - QR SCAN
+    private func handleScan(result: Result<ScanResult, ScanError>) {
+       isShowingScanner = false
+
+        switch result {
+        case .success(let result):
+            let details = result.string.components(separatedBy: "\n")
+            guard details.count == 2 else { return }
+
+            let person = Prospect(name: details[0], emailAddress: details[1], isContacted: false)
+
+            modelContext.insert(person)
+        case .failure(let error):
+            print("Scanning failed: \(error.localizedDescription)")
+        }
+    }
+
+    // MARK: - MULTI DELETE
+    private func delete() {
+        for prospect in selectedProspects {
+            modelContext.delete(prospect)
+        }
+    }
+
+    // MARK: - FILTER TYPE
+    enum FilterType {
+        case none, contacted, uncontacted
     }
 }
 
